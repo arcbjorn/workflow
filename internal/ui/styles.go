4d6c5db65a3d2e0a8def3cda58876fc136600c19
arcbjorn <arky.happ@gmail.com>
2025-10-18 17:42:46 -0300

    feat: add UI styling implementation

---
+++ internal/ui/styles.go
@@ -0,0 +1,13 @@
+package ui
+
+import (
+    "github.com/charmbracelet/lipgloss"
+)
+
+var (
+    titleStyle   = lipgloss.NewStyle().Bold(true)
+    headerStyle  = lipgloss.NewStyle().Bold(true)
+    statusStyle  = lipgloss.NewStyle().Faint(true)
+    selectedStyle = lipgloss.NewStyle().Bold(true)
+)
+
